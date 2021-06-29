f1c9d5da5bcacb653e2e366ad623982d9a445506
John Fuquene <jfuquene@email.wm.edu>
2021-06-28 23:26:10 -0400

    web nodes and random dice number

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
 let player1Score = 0 
 let player2Score = 0 
 let player1Turn = true 
+
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1 // 0-5 -> 1-6
+    console.log(randomNumber)
+})
