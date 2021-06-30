fd81b4d05a9a70efefe2aacc36c4914504560084
John Fuquene <jfuquene@email.wm.edu>
2021-06-30 13:35:28 -0400

    if else

--- index.js
+++ index.js
@@ -13,5 +13,21 @@ const resetBtn = document.getElementById("resetBtn")
 
 rollBtn.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 6) + 1 // 0-5 -> 1-6
-    console.log(randomNumber)
+    
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    player1Turn = !player1Turn
 })
