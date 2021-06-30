93b2ce527d70ef2c9af15b000d6a83e1203deacc
John Fuquene <jfuquene@email.wm.edu>
2021-06-30 13:38:47 -0400

    reset function

--- index.js
+++ index.js
@@ -44,3 +44,18 @@ rollBtn.addEventListener("click", function() {
     }
     player1Turn = !player1Turn
 })
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
